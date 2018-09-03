29352b6757866dcc95490ffaf537b10b0eb50491
ktamilara <42666475+ktamilara@users.noreply.github.com>
2018-09-03 12:17:05 +0530

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class Number {
+
+    public static void main(String[] args) {
+
+        int count = 0, num = 3452;
+
+        while(num != 0)
+        {
+            
+            num /= 10;
+            ++count;
+        }
+
+        System.out.println("Number of digits: " + count);
+    }
+}
